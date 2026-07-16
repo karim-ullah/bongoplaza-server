8a6874a4e5d3a149b09ea7703e01346ef3b5e290
karim-ullah <freelancerkarimullah@gmail.com>
2026-07-16 09:09:21 +0600

    modified

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
-import app from "./app.js";
-import { connectDb } from "./config/db.js";
-
-import config from "./config/index";
+import app from "./app";
+import config from "./config";
+import { connectDb } from "./config/db";
 
 const startServer = async () => {
-  await connectDb();
+  try {
+    await connectDb();
 
-  app.listen(config.port, () => {
-    console.log(`Server running on http://localhost:${config.port}`);
-  });
+    app.listen(config.port, () => {
+      console.log(`Server running on port ${config.port}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
 startServer();
